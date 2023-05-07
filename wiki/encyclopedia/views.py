475d0352eb7a6c4126e10f5919a6772697072038
Sérgio Garrido <124748287+garrido02@users.noreply.github.com>
2023-05-07 03:24:39 +0100

    Add files via upload

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from django import forms
+import markdown2
+from difflib import get_close_matches
+import random
+
+from . import util
+import markdown2
+
+# Index route
+def index(request):
+    # if request is post then user is searching, we need to check if page exists
+    if request.method == "POST":
+        data = request.POST
+        q = data.get("q")
+        page_entry = util.get_entry(q)
+
+        # If exists then we redirect to the page
+        if page_entry:
+            return redirect(page, q)
+        
+        # Otherwise we redirect to search route
+        else:
+            return redirect(search, q)
+    
+    # If user uses GET then display page
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+
+
+# Add page route to show a requested page with called <name>
+def page(request, name):
+    page_entry = util.get_entry(name)
+    if not page_entry:
+        return render(request, "encyclopedia/apology.html")
+    else:
+        content = markdown2.markdown(page_entry)
+        return render(request, "encyclopedia/page.html", {
+            'entry': name.capitalize(),
+            'content': content   
+        })     
+
+# Search route for a requested <search> that is not exactly a page that exists
+def search(request, s):
+    # Search current pages that are similar to the search prompt
+    entries = util.list_entries()
+    matches = get_close_matches(s, entries)
+    if matches:
+        return render(request, "encyclopedia/search.html", {
+            'matches': matches
+        })
+    else:
+        return render(request, "encyclopedia/apology.html")
+
+
+# New Page route 
+def new(request):
+    # Display page creation if user is acessing
+    if request.method == 'GET':
+        return render(request, "encyclopedia/new.html")
+    
+    # Submit form and check if page exists
+    else:
+        data = request.POST
+        title = data.get("title")
+        content = data.get("content")
+        page_entry = util.get_entry(title)
+        if page_entry:
+            return render(request, "encyclopedia/page_exists.html")
+        else:
+            util.save_entry(title, content)
+            return redirect(page, title)
+
+
+# Edit page route
+def edit(request, name):
+    # Display page edit
+    if request.method == 'GET':
+        page_entry = util.get_entry(name)
+        return render(request, "encyclopedia/edit.html", {
+            'content': page_entry,
+            'entry': name
+        })
+    
+    # If user submits a change then update
+    else:
+        data = request.POST
+        title = data.get("title")
+        content = data.get("content")
+        page_entry = util.get_entry(title)
+        util.save_entry(title, content)
+        return redirect(page, title)
+    
+
+# Def Random Page
+def random_page(request):
+    entries = util.list_entries()
+    rand = random.choice(entries)
+    return redirect(page, rand)
